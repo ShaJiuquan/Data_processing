abdd649b7b2ba79831b9511f77b780ecc9f2fbda
ShaJiuquan <126124028+ShaJiuquan@users.noreply.github.com>
2023-09-01 18:47:17 +1000

    change floder name

--- Display/display.py
+++ Display/display.py
@@ -1,6 +1,7 @@
 
 import sys
 sys.path.append("../Database_Management/")
+
 from STMdatabase import STMimage
 import matplotlib.pyplot as plt
 
@@ -22,6 +23,8 @@ class STMimagePlot(STMimage):
             image=self.get_image_value(channel=channel)
             fig,ax=plt.subplots(1,1,figsize=(7,7))
             ax.imshow(image)
+    def display_meta_data(self):
+        pass
 
 
     
